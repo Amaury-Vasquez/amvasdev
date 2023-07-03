97d30b7c73cee4654c750ff75611f8f437064291
Amaury Vasquez <amaury.vasquezenriquez@gmail.com>
2023-07-03 13:53:58 -0600

    feat: create skills constant list

--- constants/skills.ts
+++ constants/skills.ts
@@ -1,9 +1,71 @@
+import { FaLinux } from 'react-icons/fa';
+import { MdOutlineTerminal } from 'react-icons/md';
+import {
+  SiReact,
+  SiJavascript,
+  SiTypescript,
+  SiNextdotjs,
+  SiCss3,
+  SiTailwindcss,
+  SiWebpack,
+  SiPython,
+  SiPostgresql,
+  SiFastapi,
+  SiExpress,
+} from 'react-icons/si';
 import { Skill } from '../interfaces/skill';
-import { FaReact } from 'react-icons/fa';
 
 export const SKILLS: Skill[] = [
   {
     name: 'React',
-    Icon: FaReact,
+    Icon: SiReact,
+  },
+  {
+    name: 'JavaScript',
+    Icon: SiJavascript,
+  },
+  {
+    name: 'TypeScript',
+    Icon: SiTypescript,
+  },
+  {
+    name: 'NextJS',
+    Icon: SiNextdotjs,
+  },
+  {
+    name: 'css',
+    Icon: SiCss3,
+  },
+  {
+    name: 'TailwindCSS',
+    Icon: SiTailwindcss,
+  },
+  {
+    name: 'Webpack',
+    Icon: SiWebpack,
+  },
+  {
+    name: 'Python',
+    Icon: SiPython,
+  },
+  {
+    name: 'PostgreSQL',
+    Icon: SiPostgresql,
+  },
+  {
+    name: 'FastAPI',
+    Icon: SiFastapi,
+  },
+  {
+    name: 'Express',
+    Icon: SiExpress,
+  },
+  {
+    name: 'Linux',
+    Icon: FaLinux,
+  },
+  {
+    name: 'CLI',
+    Icon: MdOutlineTerminal,
   },
 ];
