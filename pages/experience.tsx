3734efb79982e8661a9b4153cc68a8918339dfed
Amaury Vasquez <amaury.vasquezenriquez@gmail.com>
2023-07-05 13:00:33 -0600

    fix: build process

---
+++ pages/experience.tsx
@@ -0,0 +1,15 @@
+import Head from 'next/head';
+
+export default function ExperiencePage() {
+  return (
+    <>
+      <Head>
+        <title> Amaury Vasquez - Laboral Experience</title>
+        <meta
+          name="description"
+          content="Amaury Vasquez - Laboral Experience. React frontend developer."
+        />
+      </Head>
+    </>
+  );
+}
