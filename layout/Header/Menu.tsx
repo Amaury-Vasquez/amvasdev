0c0d69d3c010e4a5255553bed43dbf58688a8012
Amaury Vasquez <amaury.vasquezenriquez@gmail.com>
2023-07-29 14:42:20 -0600

    fix: style issues

--- layout/Header/Menu.tsx
+++ layout/Header/Menu.tsx
@@ -1,6 +1,8 @@
+import clsx from 'clsx';
 import { FC } from 'react';
 import { MdMenu } from 'react-icons/md';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 import { useToggle, useOnClickOutside } from '@/hooks';
 import { IconButton } from '@/components';
 import { PAGE_LINKS } from '@/constants';
@@ -8,6 +10,7 @@ import { PAGE_LINKS } from '@/constants';
 const Menu: FC = () => {
   const { isActive, toggle, deactivate } = useToggle(false);
   const ref = useOnClickOutside<HTMLDivElement>(deactivate);
+  const { pathname } = useRouter();
 
   return (
     <div className="dropdown" ref={ref}>
@@ -18,13 +21,30 @@ const Menu: FC = () => {
       />
       {isActive && (
         <ul className="menu menu-sm absolute mt-3 z-[1] p-2 shadow-lg shadow-base-300 bg-base-100 rounded-box w-52">
-          {PAGE_LINKS.map(({ name, url }, idx) => (
-            <li className="py-1" key={'headernavbarlink:' + idx}>
-              <Link className="hover:text-primary font-semibold" href={url}>
-                {name}
-              </Link>
-            </li>
-          ))}
+          {PAGE_LINKS.map(({ name, url }, idx) => {
+            const disabledPath = url === pathname;
+            return (
+              <li
+                className={clsx(
+                  'py-1',
+                  disabledPath && 'disabled !cursor-text'
+                )}
+                key={'headernavbarlink:' + idx}
+              >
+                {disabledPath ? (
+                  <span className="font-semibold"> {name} </span>
+                ) : (
+                  <Link
+                    className="hover:text-primary font-semibold"
+                    onClick={deactivate}
+                    href={url}
+                  >
+                    {name}
+                  </Link>
+                )}
+              </li>
+            );
+          })}
         </ul>
       )}
     </div>
--- layout/index.tsx
+++ layout/index.tsx
@@ -37,7 +37,7 @@ export default function AppLayout({ children }: { children: React.ReactNode }) {
                 : '/'
             }
             Icon={MdOutlineNavigateNext}
-            className="fixed lg:top-1/2 right-2 z-50 text-primary lg:w-12 lg:h-12 w-10 h-10 xxs:top-[72px] top-2 bg-base-200 xs:top-20 xxs:bg-base-100 duration-1000"
+            className="fixed lg:top-1/2 right-2 z-40 text-primary lg:w-12 lg:h-12 w-10 h-10 xxs:top-[72px] top-2 bg-base-200 xs:top-20 xxs:bg-base-100 duration-1000"
           />
         )}
       <div className="bg-base-200 max-xxs:min-h-xxs-screen max-xxs:mt-28 min-h-content h-fit mt-16">
